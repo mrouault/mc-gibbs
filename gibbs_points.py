5671b58ce410fed3a8603ed45d408f183daef92b
mrouault <rouault.martin@protonmail.com>
2025-04-28 18:32:15 +0200

    Sampling scripts

---
+++ gibbs_points.py
@@ -0,0 +1,62 @@
+from typing import Callable
+from jax import numpy as jnp
+from jax import scipy
+from jax import random, Array, jit, vmap, grad
+from jax.tree_util import Partial as partial
+from jax.lax import fori_loop, cond, dynamic_slice
+import numpyro
+import optax
+import jax
+from mcmc_samplers import mh, mala
+
+class gibbs(numpyro.distributions.Distribution) :
+
+    def __init__(self, d, n, K, beta_n, V = None) :
+        '''
+        :param K : translation invariant kernel function K : R^d x R^d -> R, might be infinite on the diagonal.
+        '''
+
+        self.n = n
+        self.K = K
+        self.V = V
+        self.d = d
+        self.beta_n = beta_n
+        event_shape = (n, d)
+        super(gibbs, self).__init__(event_shape = event_shape)
+
+
+    def log_prob(self, value) :
+
+        #value = (x_1, \dots, x_n)
+        value_shape = value.shape #should be (1, n*d)
+        X = jnp.array(value.reshape(self.d, self.n))
+        index = jnp.array([k for k in range(self.n)])
+
+        k_inter = lambda j ,k : jnp.where(j == k, 0., self.K(X[:, j], X[:, k])) #doesn't count diagonal terms
+        pair_j = lambda j : jit(vmap(partial(k_inter, j)))(index).sum()
+        pair_jit = jit(vmap(pair_j))(index).sum()
+        #pair_inter = jnp.log(self.beta_N) +jnp.log(pair_jit) -jnp.log(2) - 2*jnp.log(self.N)
+        pair_inter = self.beta_n * (pair_jit )/  (2*self.n**2)
+
+        v_inter = lambda j : self.V(X[:, j])
+        ext_jit = jit(vmap(v_inter))(index).sum()
+        exter_V = self.beta_n * ext_jit / self.n
+
+        return - pair_inter - exter_V
+        #return  -jnp.exp(exter_V)*(jnp.exp(pair_inter - exter_V) +1)
+
+    def sample(self, key, start_sample, n_iter, step_size, method = 'mala') :
+        '''
+        :start_sample : Array with the first element with shape (1, n*d).
+        return : Dic with last sample of the chain with shape (1, n*d) and the acceptance rate.
+        Sample should be reshaped as (d, n).T to get the shape (n, d).
+        '''
+        start_sample = jnp.atleast_2d(start_sample)
+        target_mala = lambda x : self.log_prob(x)
+        sample_mala, acceptance = jit(vmap(partial(mala,
+                               log_prob_target = target_mala,
+                               n_iter = n_iter,
+                               step_size =  step_size)))(random.split(key, 1), start_sample)
+        return {"samples": sample_mala, "acceptance": acceptance}
+    
+    #mettre des logsumpexp pour l'instabilité
---
+++ mcmc_samplers.py
@@ -0,0 +1,104 @@
+from typing import Callable
+from jax import numpy as jnp
+from jax import scipy
+from jax import random, Array, jit, vmap, grad
+from jax.tree_util import Partial as partial
+from jax.lax import fori_loop, cond, dynamic_slice
+import numpyro
+import optax
+import jax
+
+def mh(key: Array,
+         start_sample: Array,
+         log_prob_target: Callable[[jnp.ndarray], jnp.ndarray],
+         n_iter: int,
+         step_size: float) -> jnp.ndarray:
+    '''
+    This function implements MALA sampler.
+    :param key: PRNGArray specifying the key that is used for the random numbers
+    :param start_sample: Array with the first element with shape (n_chains, d) where d is the dimension of the target distribution and n_chains is the number of Markov chains.
+    :param log_prob_target: Function that calculates the log probability of the target distribution at a given point.
+    :param n_iter: int The number of steps of the chain.
+    :param step_size: The step size for the Langevin kernel.
+    :return: The samples along the chain and the acceptance rate computed online.
+    '''
+    start_sample = jnp.atleast_2d(start_sample)
+
+    def mh_step(i, val):
+
+        positions, key, acceptance = val
+        sample = jnp.atleast_2d(positions[i-1, :])
+        key, subkey_u, subkey_sample = random.split(key, 3)
+        #Sample the langevin kernel
+        noise = random.normal(subkey_sample, (sample.shape[-1],))
+        next = sample + jnp.sqrt(2 * step_size) * noise
+        #Compute the log acceptance ratio
+        fwd_ker_logprob = - (noise ** 2).sum() / 2
+        bwd_ker_logprob = - ((sample - next)**2).sum() / (4 * step_size)
+        log_ratio = log_prob_target(next) + bwd_ker_logprob - log_prob_target(sample) - fwd_ker_logprob
+
+        log_u = jnp.log(random.uniform(subkey_u))
+        accept = log_ratio > log_u
+        x = jnp.where(log_ratio > log_u, next, sample)
+
+        acceptance = (i*acceptance + accept)/(i+1)
+        positions = positions.at[i, :].set(jnp.atleast_2d(x)[0, :])
+        return (positions, key, acceptance)
+
+    all_positions = jnp.zeros((n_iter, start_sample.shape[1]))
+    all_positions.at[0, :].set(start_sample[0, :])
+
+    positions, key, acceptance = fori_loop(1,
+                          n_iter,
+                          body_fun=mh_step,
+                          init_val=(all_positions, key, jnp.array([0.])))
+    return positions, acceptance
+
+def mala(key: Array,
+         start_sample: Array,
+         log_prob_target: Callable[[jnp.ndarray], jnp.ndarray],
+         n_iter: int,
+         step_size: float) -> jnp.ndarray:
+    '''
+    This function implements MALA sampler.
+    :param key: PRNGArray specifying the key that is used for the random numbers
+    :param start_sample: Array with the first element with shape (1, d) where d is the dimension of the target distribution.
+    :param log_prob_target: Function that calculates the log probability of the target distribution at a given point.
+    :param n_iter: int The number of steps of the chain.
+    :param step_size: The step size for the Langevin kernel.
+    :return: The last sample of the chain and the acceptance rate computed online.
+    '''
+    start_sample = jnp.atleast_2d(start_sample)
+    #you need to define the gradient of the log_prob_target parameter
+    grad_logpdf = grad(lambda x: log_prob_target(x))
+
+    def mh_step(i, val):
+
+        sample, key, acceptance = val
+        key, subkey_u, subkey_sample = random.split(key, 3)
+        #Sample the langevin kernel
+
+        noise = random.normal(subkey_sample, (sample.shape[-1],))
+        next = sample + step_size * grad_logpdf(sample) \
+               + jnp.sqrt(2 * step_size) * noise
+        #Compute the log acceptance ratio
+        fwd_ker_logprob = - (noise ** 2).sum() / 2
+        bwd_ker_logprob = - ((sample - next - step_size * grad_logpdf(next))**2).sum() / (4 * step_size)
+        log_ratio = log_prob_target(next) + bwd_ker_logprob - log_prob_target(sample) - fwd_ker_logprob
+
+        log_u = jnp.log(random.uniform(subkey_u))
+        accept = log_ratio > log_u
+        acceptance = (i*acceptance + accept)/(i+1)
+
+        x = cond(accept,
+                 lambda _: next,
+                 lambda _: sample,
+                 None)
+        return (x, key, acceptance)
+
+    sample, k, acceptance = fori_loop(0,
+                          n_iter,
+                          body_fun=mh_step,
+                          init_val= (start_sample, key, jnp.array([0.])))
+    sample = jnp.atleast_2d(sample)
+    return sample, acceptance
